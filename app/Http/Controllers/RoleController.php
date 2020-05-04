12d643a0e1db56ac3887e1184cdafed502874b35
jhonatanfdez <jeremiselxi@gmail.com>
2020-05-04 09:54:08 -0400

    09 Metodos edit y update

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -77,9 +77,25 @@ class RoleController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
-    {
-        //
+    public function edit(Role $role)
+    {   
+
+        $permission_role=[];
+
+        foreach($role->permissions as $permission) {
+            $permission_role[]=$permission->id; 
+        }
+        //return   $permission_role;
+
+
+        //return $role;
+        $permissions = Permission::get();
+
+
+
+
+        return view('role.edit', compact('permissions','role','permission_role'));
+        
     }
 
     /**
@@ -89,9 +105,22 @@ class RoleController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Role $role)
     {
-        //
+        $request->validate([
+            'name'          => 'required|max:50|unique:roles,name,'.$role->id,
+            'slug'          => 'required|max:50|unique:roles,slug,'.$role->id,
+            'full-access'   => 'required|in:yes,no'
+        ]);
+
+        $role->update($request->all());
+        
+        if ($request->get('permission')) {
+            //return $request->all();
+            $role->permissions()->sync($request->get('permission'));
+        }
+        return redirect()->route('role.index')
+            ->with('status_success','Role updated successfully'); 
     }
 
     /**
